b9db82bdcd1b3b6bf27a7efee6656ef111133918
dantauu <azazzzz455u@gmail.com>
2025-01-20 23:35:55 +0300

    add calendar file

--- src/features/ui/task-title/task-title.tsx
+++ src/features/ui/task-title/task-title.tsx
@@ -1,6 +1,5 @@
 import AddTaskBtn from "@/shared/ui/add-task-text/add-task-text"
 import style from './task-title.module.scss'
-// import '../../../app/styles/index.scss'
 
 const TaskTitle = () => {
     return (
