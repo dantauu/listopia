6d4fcc7990169eec69a3bcfd81a23172d84f0940
dantauu <azazzzz455u@gmail.com>
2025-01-21 22:35:11 +0300

    add new title

--- src/features/ui/task-title/task-title.tsx
+++ src/features/ui/task-title/task-title.tsx
@@ -4,14 +4,35 @@ import style from './task-title.module.scss'
 const TaskTitle = () => {
     return (
 			<div className='container'>
-				<AddTaskBtn
-					addTaskColor={style.colorTask}
-					addTaskText='Личные желания'
-					addTaskTextClass={style.colorTaskText}
-                    addTaskWrapper={style.titleTaskWrapper}
-				/>
+				<div className={style.wrapperTitleTask}>
+					<div className='title__task-item'>
+						<AddTaskBtn
+							addTaskColor={style.colorTask}
+							addTaskText='Личные желания'
+							addTaskTextClass={style.colorTaskText}
+							addTaskWrapper={style.titleTaskWrapper}
+						/>
+					</div>
+					<div className='title__task-item'>
+						<AddTaskBtn
+							addTaskColor={style.colorTask}
+							addTaskText='Необходимые'
+							addTaskTextClass={style.colorTaskText}
+							addTaskWrapper={style.titleTaskWrapper}
+						/>
+					</div>
+					<div className='title__task-item'>
+						<AddTaskBtn
+							addTaskColor={style.colorTask}
+							addTaskText='Подарки'
+							addTaskTextClass={style.colorTaskText}
+							addTaskWrapper={style.titleTaskWrapper}
+						/>
+					</div>
+				</div>
 			</div>
 		)
 }
+
 
 export default TaskTitle
