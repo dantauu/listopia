853604082c56c52817d9c3ebe565289678ec624a
dantauu <azazzzz455u@gmail.com>
2025-04-20 23:54:49 +0300

    refucktour

--- src/features/ui/modals/modals.tsx
+++ src/features/ui/modals/modals.tsx
@@ -1,23 +1,22 @@
-'use client'
+"use client"
 
 import { useAppSelector } from "@/redux/hooks"
 import ModalAddTask from "@/shared/ui/modal-add-task/modal-add-task"
 import { AnimatePresence } from "framer-motion"
 
-
 const Modals = () => {
-   const { isOpen, currentView } = useAppSelector((state) => state.modalTask)
-   return (
-      <>
-         <AnimatePresence>
-            {isOpen && currentView && (
-               <div className="">
-                  {<ModalAddTask key={'task-modal'} view={currentView} />}
-               </div>
-            )}
-         </AnimatePresence>
-      </>
-   )
+  const { isOpen, currentView } = useAppSelector((state) => state.modalTask)
+  return (
+    <>
+      <AnimatePresence>
+        {isOpen && currentView && (
+          <div className="">
+            {<ModalAddTask key={"task-modal"} view={currentView} />}
+          </div>
+        )}
+      </AnimatePresence>
+    </>
+  )
 }
 
 export default Modals
--- src/intities/ui/task-component/task-component.tsx
+++ src/intities/ui/task-component/task-component.tsx
@@ -1,28 +1,24 @@
 import { TaskProps } from "@/redux/slices/task-component-slice"
-import style from './task-component.module.scss'
+import style from "./task-component.module.scss"
 
-const TaskComponent = ({ task }: {task: TaskProps}) => {
-   return (
-      <div className={style.taskCard}>
-         <div className={style.taskTitle}>
-            <h3 className={style.taskTitleInner}>
-               {task.title}
-            </h3>
-         </div>
-         {task.description && (
-            <div className={style.description}>
-               <p className={style.descriptionInner}>
-                  {task.description}
-               </p>
-            </div>
-         )}
-         {task.image && (
-            <div className={style.image}>
-               <img src={task.image} className={style.taskImage} alt="" />
-            </div>
-         )}
+const TaskComponent = ({ task }: { task: TaskProps }) => {
+  return (
+    <div className={style.taskCard}>
+      <div className={style.taskTitle}>
+        <h3 className={style.taskTitleInner}>{task.title}</h3>
       </div>
-   )
+      {task.description && (
+        <div className={style.description}>
+          <p className={style.descriptionInner}>{task.description}</p>
+        </div>
+      )}
+      {task.image && (
+        <div className={style.image}>
+          <img src={task.image} className={style.taskImage} alt="" />
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default TaskComponent
