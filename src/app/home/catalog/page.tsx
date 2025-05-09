a61c29af1253a37caac23053947f548485d97dcf
dantauu <azazzzz455u@gmail.com>
2025-05-09 23:09:08 +0300

    start prettier

--- src/app/home/catalog/page.tsx
+++ src/app/home/catalog/page.tsx
@@ -1,28 +1,28 @@
-'use client'
+"use client"
 
-import TaskTitleUi from '@/widgets/task-title/ui'
-import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
-import style from './catalog.module.scss'
+import TaskTitleUi from "@/widgets/task-title/ui"
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
+import style from "./catalog.module.scss"
 
 const CatalogPage = () => {
-	const pathname = usePathname()
-	const searchParams = useSearchParams()
-	const [isActive, setIsActive] = useState(false)
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const [isActive, setIsActive] = useState(false)
 
-	useEffect(() => {
-		setIsActive(true)
-		setIsActive(false)
-		const timeout = setTimeout(() => setIsActive(true), 0)
+  useEffect(() => {
+    setIsActive(true)
+    setIsActive(false)
+    const timeout = setTimeout(() => setIsActive(true), 0)
 
-		return () => clearTimeout(timeout)
-	}, [pathname, searchParams])
+    return () => clearTimeout(timeout)
+  }, [pathname, searchParams])
 
-	return (
-		<div className={`${style.pageTransition} ${isActive && style.active}`}>
-			<TaskTitleUi />
-		</div>
-	)
+  return (
+    <div className={`${style.pageTransition} ${isActive && style.active}`}>
+      <TaskTitleUi />
+    </div>
+  )
 }
 
 export default CatalogPage
